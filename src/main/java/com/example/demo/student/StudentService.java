7d1f2f8e1a67928036a100db08ed8ced06f7af62
Brendan James Mulhern <brendanjamesmulhern@gmail.com>
2022-04-15 16:16:51 -0400

    deploy

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,17 @@
+package com.example.demo.student;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
