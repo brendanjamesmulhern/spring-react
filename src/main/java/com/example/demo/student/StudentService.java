e2954b3e51b0790d1a949f9d60f0d9e679662a0d
Brendan James Mulhern <brendanjamesmulhern@gmail.com>
2022-04-15 16:43:14 -0400

    deploy

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -14,4 +15,21 @@ public class StudentService {
     List<Student> getAllStudents() {
         return studentRepository.findAll();
     }
+
+    public void addStudent(Student student) {
+        // check if email is taken
+        studentRepository.save(student);
+    };
+
+    public void updateStudent(Student student) {
+        studentRepository.findById(student.getId()).map(studentFromDB -> {
+                studentFromDB = student;
+                studentRepository.save(studentFromDB);
+                return null;
+        });
+    };
+
+    public void deleteStudent(Student student) {
+        studentRepository.deleteById(student.getId());
+    };
 }
