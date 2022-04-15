fedb03fa594982ba2b99f703e76c6b693bbf9c99
Brendan James Mulhern <brendanjamesmulhern@gmail.com>
2022-04-15 17:16:43 -0400

    deploy

---
+++ src/main/resources/static/MMkeli9eogKIbWdNo7QUU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/957-2b50d51154eafec9.js","static/chunks/pages/index-cea47c1ac45487d2.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
